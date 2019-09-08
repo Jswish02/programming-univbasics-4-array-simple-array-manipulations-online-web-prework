a8eb37fd60782f0712b62ead50dba0a0d4c663bb
Joanh Brown <jswish02@gmail.com>
2019-09-08 18:14:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++
@@ -1,9 +0,0 @@
- colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
- colors_in_the_rainbow.push (violet)
- end
-
-describe "using_unshift" do 
-  before(:each) do 
-    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    bouroughs_in_nyc.unshift (staten island)
-  
